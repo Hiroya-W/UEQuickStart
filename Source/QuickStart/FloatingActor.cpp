def0921e875f028e4d4fa30e1c8beb88d1646230
Hiroya-W <hyuyukun@gmail.com>
2025-06-02 00:33:44 +0900

    :tada: Initial Commit

---
+++ Source/QuickStart/FloatingActor.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2025, Hiroya-W All rights reserved.
+
+
+#include "FloatingActor.h"
+
+// Sets default values
+AFloatingActor::AFloatingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	VisualMesh->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> CubeVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Cube.Shape_Cube"));
+
+	if (CubeVisualAsset.Succeeded())
+	{
+		VisualMesh->SetStaticMesh(CubeVisualAsset.Object);
+		VisualMesh->SetRelativeScale3D(FVector(0.0f, 0.0f, 0.0f));
+	}
+
+}
+
+// Called when the game starts or when spawned
+void AFloatingActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloatingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector NewLocation = GetActorLocation();
+	FRotator NewRotation = GetActorRotation();
+	const float RunningTime = GetGameTimeSinceCreation();
+	const float DeltaHeight = (FMath::Sin(RunningTime + DeltaTime) - FMath::Sin(RunningTime));
+	NewLocation.Z += DeltaHeight * FloatSpeed;
+	const float DeltaRotation = DeltaTime * RotationSpeed;
+	NewRotation.Yaw += DeltaRotation;
+	SetActorLocationAndRotation(NewLocation, NewRotation);
+}
+
---
+++ Source/QuickStart/FloatingActor.h
@@ -0,0 +1,35 @@
+// Copyright (c) 2025, Hiroya-W All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloatingActor.generated.h"
+
+UCLASS()
+class QUICKSTART_API AFloatingActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloatingActor();
+	
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* VisualMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="FloatingActor")
+	float FloatSpeed = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="FloatingActor")
+	float RotationSpeed = 20.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
