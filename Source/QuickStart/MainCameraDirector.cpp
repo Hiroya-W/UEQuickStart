c6e28c228c632a803109d4639b387ffd80f29d5d
Hiroya-W <hyuyukun@gmail.com>
2025-06-02 02:09:35 +0900

    :sparkles: ゲームのメインカメラとしてセットする

---
+++ Source/QuickStart/MainCameraDirector.cpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2025, Hiroya-W All rights reserved.
+
+
+#include "MainCameraDirector.h"
+
+// Sets default values
+AMainCameraDirector::AMainCameraDirector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMainCameraDirector::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (APlayerController* PlayerController = GetWorld()->GetFirstPlayerController())
+	{
+		PlayerController->SetViewTarget(CameraActor);
+	}
+	
+}
+
+// Called every frame
+void AMainCameraDirector::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/QuickStart/MainCameraDirector.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2025, Hiroya-W All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "MainCameraDirector.generated.h"
+
+UCLASS()
+class QUICKSTART_API AMainCameraDirector : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMainCameraDirector();
+
+	UPROPERTY(EditAnywhere, Category = "Camera")
+	AActor* CameraActor;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
