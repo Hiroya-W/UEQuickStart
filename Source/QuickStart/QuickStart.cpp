d10978ffada7007551d7e4939b5ebe2cea1144a8
Hiroya-W <hyuyukun@gmail.com>
2025-06-03 02:24:18 +0900

    :recycle: format

--- Source/QuickStart/QuickStart.cpp
+++ Source/QuickStart/QuickStart.cpp
@@ -3,4 +3,4 @@
 #include "QuickStart.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, QuickStart, "QuickStart" );
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, QuickStart, "QuickStart");
