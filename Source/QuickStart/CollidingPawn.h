61b6ecdbe4e1203b8fb961bea9fad5f1e0fcaa49
Hiroya-W <hyuyukun@gmail.com>
2025-06-04 01:23:07 +0900

    :sparkles: CollidingPawnを作成

---
+++ Source/QuickStart/CollidingPawn.h
@@ -0,0 +1,29 @@
+// Copyright (c) 2025, Hiroya-W All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+
+#include "CollidingPawn.generated.h"
+
+UCLASS()
+class QUICKSTART_API ACollidingPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ACollidingPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
