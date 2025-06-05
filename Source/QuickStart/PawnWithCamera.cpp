d6130a0e2484a6916728060ac2820181ccde71e1
Hiroya-W <hyuyukun@gmail.com>
2025-06-06 01:41:03 +0900

    :sparkles: Playerが操作できるクラスを追加

--- Source/QuickStart/PawnWithCamera.cpp
+++ Source/QuickStart/PawnWithCamera.cpp
@@ -3,11 +3,40 @@
 
 #include "PawnWithCamera.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
 // Sets default values
 APawnWithCamera::APawnWithCamera()
 {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	StaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMeshComponent"));
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+
+	StaticMeshComponent->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAssset(
+		TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAssset.Succeeded())
+	{
+		StaticMeshComponent->SetStaticMesh(SphereVisualAssset.Object);
+		StaticMeshComponent->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		StaticMeshComponent->SetWorldScale3D(FVector(0.8f));
+	}
+
+	SpringArmComponent->SetupAttachment(StaticMeshComponent);
+	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+
+	SpringArmComponent->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 50.0f), FRotator(-60.0f, 0.0f, 0.0f));
+	SpringArmComponent->TargetArmLength = 400.0f;
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->CameraLagSpeed = 3.0f;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
@@ -20,10 +49,93 @@ void APawnWithCamera::BeginPlay()
 void APawnWithCamera::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	//[ZoomIn (ズームイン)] ボタンが押されていればズームインし、押されていなければズームアウトします
+	{
+		if (bZoomingIn)
+		{
+			ZoomFactor += DeltaTime / 0.5f; // 0.5 秒間ズームインします
+		}
+		else
+		{
+			ZoomFactor -= DeltaTime / 0.25f; // 0.25 秒間ズームアウトします
+		}
+		ZoomFactor = FMath::Clamp<float>(ZoomFactor, 0.0f, 1.0f);
+
+		// ZoomFactor に応じて、カメラの FOV と SpringArm の長さをブレンドします
+		CameraComponent->FieldOfView = FMath::Lerp<float>(90.0f, 60.0f, ZoomFactor);
+		SpringArmComponent->TargetArmLength = FMath::Lerp<float>(400.0f, 300.0f, ZoomFactor);
+	}
+	// アクタのヨーを回転させます。アタッチされているため、カメラも回転します
+	{
+		FRotator NewRotation = GetActorRotation();
+		NewRotation.Yaw += CameraInput.X;
+		SetActorRotation(NewRotation);
+	}
+
+	// カメラのピッチを回転させますが、常に下を向くよう制限します
+	{
+		FRotator NewRotation = SpringArmComponent->GetComponentRotation();
+		NewRotation.Pitch = FMath::Clamp(NewRotation.Pitch + CameraInput.Y, -80.0f, -15.0f);
+		SpringArmComponent->SetWorldRotation(NewRotation);
+	}
+	// 「MoveX」軸と「MoveY」軸に応じた動きを扱います
+	{
+		if (!MovementInput.IsZero())
+		{
+			// 移動の入力軸の値を毎秒 100 単位でスケーリングします
+			MovementInput = MovementInput.GetSafeNormal() * 100.0f;
+			FVector NewLocation = GetActorLocation();
+			NewLocation += GetActorForwardVector() * MovementInput.X * DeltaTime;
+			NewLocation += GetActorRightVector() * MovementInput.Y * DeltaTime;
+			SetActorLocation(NewLocation);
+		}
+	}
 }
 
 // Called to bind functionality to input
 void APawnWithCamera::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// 「ZoomIn」にイベントを接続します
+	InputComponent->BindAction("ZoomIn", IE_Pressed, this, &APawnWithCamera::ZoomIn);
+	InputComponent->BindAction("ZoomIn", IE_Released, this, &APawnWithCamera::ZoomOut);
+
+	// 4 軸の各フレーム処理に接続します
+	InputComponent->BindAxis("MoveForward", this, &APawnWithCamera::MoveForward);
+	InputComponent->BindAxis("MoveRight", this, &APawnWithCamera::MoveRight);
+	InputComponent->BindAxis("CameraPitch", this, &APawnWithCamera::PitchCamera);
+	InputComponent->BindAxis("CameraYaw", this, &APawnWithCamera::YawCamera);
+}
+
+// 入力関数
+void APawnWithCamera::MoveForward(float AxisValue)
+{
+	MovementInput.X = FMath::Clamp<float>(AxisValue, -1.0f, 1.0f);
+}
+
+void APawnWithCamera::MoveRight(float AxisValue)
+{
+	MovementInput.Y = FMath::Clamp<float>(AxisValue, -1.0f, 1.0f);
+}
+
+void APawnWithCamera::PitchCamera(float AxisValue)
+{
+	CameraInput.Y = AxisValue;
+}
+
+void APawnWithCamera::YawCamera(float AxisValue)
+{
+	CameraInput.X = AxisValue;
+}
+
+void APawnWithCamera::ZoomIn()
+{
+	bZoomingIn = true;
+}
+
+void APawnWithCamera::ZoomOut()
+{
+	bZoomingIn = false;
 }
--- Source/QuickStart/PawnWithCamera.h
+++ Source/QuickStart/PawnWithCamera.h
@@ -20,6 +20,29 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class USpringArmComponent* SpringArmComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	class UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* StaticMeshComponent;
+
+	// 入力変数
+	FVector2D MovementInput;
+	FVector2D CameraInput;
+	float ZoomFactor;
+	bool bZoomingIn;
+
+	// 入力関数
+	void MoveForward(float AxisValue);
+	void MoveRight(float AxisValue);
+	void PitchCamera(float AxisValue);
+	void YawCamera(float AxisValue);
+	void ZoomIn();
+	void ZoomOut();
+
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
